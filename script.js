9e61011c52fedc5ada329b287f4d8f3a939a0c11
petermbiyu <162939527+petermbiyu@users.noreply.github.com>
2024-10-06 14:06:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed (".text", {
+    strings: ["frontend developer", "web designer", "teacher"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    backDelay: 1000
+});
