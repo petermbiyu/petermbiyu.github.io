2847541f061f0d085d02031d25ace94537a893f0
petermbiyu <162939527+petermbiyu@users.noreply.github.com>
2024-10-08 14:37:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ let typed = new Typed (".text", {
     loop: true,
     backDelay: 1000
 });
+
+/* let slider = tns({
+    container: ".project-list",
+    "slideBy": "1",
+    "speed" : 400,
+    "nav" : false,
+    autoplay: true,
+    controls : false,
+    autoplayButtonOutput : false,
+    responsive : {
+        1650 : {
+            items : 4,
+            gutter : 20
+        },
+        1024 : {
+            items : 3,
+            gutter : 20
+        },
+        768 : {
+            items : 2,
+            gutter : 20
+        },
+        480 : {
+            items : 1
+        }
+    }
+}); */
+
+/* scroll */
+const prev = document.getElementById("btn-lft");
+const next = document.getElementById("btn-rgt");
+const itemList = document.getElementById("item-list");
+const itemWidth = 350;
+const gap = 20;
+
+prev.addEventListener("click", () => {
+    itemList.scrollLeft -= (itemWidth + gap)
+});
+next.addEventListener("click", () => {
+    itemList.scrollLeft += (itemWidth + gap)
+});
